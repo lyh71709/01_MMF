d770d27ac8298aceb500c6255ebb6bb355ec1609
lyh71709 <60583161+lyh71709@users.noreply.github.com>
2021-02-18 13:14:34 +1300

    Finished Checkpoint 2

--- 00_MMF_base_v1.py
+++ 00_MMF_base_v1.py
@@ -36,26 +36,27 @@ def int_check(question):
 # Main rountine
 
 #Set up dictionaries / lists needed to hold data
-name = ""
-count = 0
 MAX_TICKETS = 5
+name = ""
+ticket_count = 0
+ticket_sales = 0
 
 # Ask user for if they have used the program before and show how to use it
 
 # Loop to get ticket details
 
 # Get name (can't be blank)
-while name != "xxx" and count < MAX_TICKETS:
+while name != "xxx" and ticket_count < MAX_TICKETS:
 
     # Makes sure there is no plural when there is 1 ticket left
-    if MAX_TICKETS - count == 1:
+    if MAX_TICKETS - ticket_count == 1:
         print("There is 1 seat left")
     else:
-        print("You have {} seats left".format(MAX_TICKETS - count))
+        print("You have {} seats left".format(MAX_TICKETS - ticket_count))
 
     # Get details
     name = not_blank("What is your name? ", "Sorry - this can't be blank, please enter your name")
-    # Makes xxx not count as a ticket sold
+    # Makes xxx not ticket_count as a ticket sold
     if name == "xxx":
         print()
         break
@@ -73,29 +74,36 @@ while name != "xxx" and count < MAX_TICKETS:
         print()
         continue
 
-    count += 1
-    print()
+    # Calculate ticket price
+    if age < 16:
+        ticket_price = 7.5
+    elif age < 65:
+        ticket_price = 10.5
+    else:
+        ticket_price = 6.5
+
+    print("{} : ${:.2f} \n".format(name, ticket_price))
+
+    ticket_count += 1
+    ticket_sales += ticket_price
+
+# Calculate ticket profit
+ticket_profit = ticket_sales - (5 * ticket_count)
 
 
 # Gives feedback after loop has ended
-if count == MAX_TICKETS:
+if ticket_count == MAX_TICKETS:
     print("You have sold all the available tickets!")
+    print("Profit from Tickets: ${:.2f}".format(ticket_profit))
 else:
     print("You have sold {} tickets. \n"
-          "There are {} places still available".format(count, MAX_TICKETS - count))
-
-
-
-    # Calculate ticket price
+          "There are {} places still available".format(ticket_count, MAX_TICKETS - ticket_count))
 
     # Loop to ask for snacks
 
     # Calculate snack price
 
     # Ask for payment method (and apply surcharge if needed)
-
-
-# Calculate Total sales and profit
 
 # Output data to text file
 
